d0f245aaf7383e6afe055688c37c22cb9cb132e6
tim840818 <51027715+tim840818@users.noreply.github.com>
2024-06-23 01:21:05 +0200

    small change

--- ml_model.py
+++ ml_model.py
@@ -1,11 +1,3 @@
-# from keras.models import Sequential
-# from keras.layers.core import Dense, Dropout, Activation
-
-# #for CNN model
-# from keras.layers import Flatten
-# from keras.layers import Conv2D, MaxPooling2D
-
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.callbacks import EarlyStopping
